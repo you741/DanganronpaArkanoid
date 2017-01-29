54ac3cb26b1d8b0681a4412d4b538c4dd6c1a083
You Zhou <youzhouphoenix@hotmail.com>
2017-01-29 18:09:38 -0500

    Added some stuff
    -Victory and dead screen
    -Pause button
    -Nice background scrolling
    -Slow and Fast effect

--- ArkanoidProject/core/src/com/zhou/arkanoid/Arkanoid.java
+++ ArkanoidProject/core/src/com/zhou/arkanoid/Arkanoid.java
@@ -22,6 +22,8 @@ import java.util.*;
  */
 
 public class Arkanoid extends ApplicationAdapter implements InputProcessor{
+	public static final int NUM_LEVELS = 2;
+	
 	SpriteBatch batch;
 	Ball ball;
 	ArrayList<Ball> extraBalls; //extra balls from bonus brick
@@ -40,24 +42,32 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 	Texture lifeEffectSym;
 	Texture bigEffectSym;
 	Texture smallEffectSym;
+	Texture fastEffectSym;
+	Texture slowEffectSym;
+	Texture[] backgrounds;
+	Texture pausedSign;
+	Texture vicScreen;
+	Texture deadScreen;
 	Sprite ballSprite;
 	Sprite platSprite;
 	
 	double platx,platy,platwidth,platheight; //platform x and y and width and height
 	Rectangle platRect;
+	
 	boolean onPlat; //is ball sitting on the platform?
+	boolean paused; //is game paused?
 	
 	int lives;
 	int level;
 	int time; //time elapsed in ticks
 	int savePoint; //savePoint is a counter for time, for time based events
-	int spd; //speed of ball
 	int quota; //amount of bricks that need to be left at most to win
+	int numBricks; //number of bricks the level had originally
 	
 	String mode; //which mode the game is in (menu, game, dead, victory)
 	
 	Music music;
-	Sound sound;
+	Sound brickHit;
 	
 	//Rectangles
 	Rectangle playRect;
@@ -81,7 +91,8 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 			music.setLooping(true);
 			music.play();
 		}
-		
+		//SOUND FX
+		brickHit = Gdx.audio.newSound(Gdx.files.internal("BrickHit.mp3"));
 		//sets important textures
 		brickSprites = new Texture[7];
 		String[] brickTextSrc = new String[]{"Red","Red","Yellow","Green","Cyan","Blue","Strong"};
@@ -95,6 +106,15 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 		lifeEffectSym = new Texture("lifeSymbol.png");
 		bigEffectSym = new Texture("bigSymbol.png");
 		smallEffectSym = new Texture("smallSymbol.png");
+		fastEffectSym = new Texture("fastSymbol.png");
+		slowEffectSym = new Texture("slowSymbol.png");
+		pausedSign = new Texture("pausedSign.png");
+		backgrounds = new Texture[NUM_LEVELS];
+		for(int i = 0;i < NUM_LEVELS;i++){
+			backgrounds[i] = new Texture("level"+(i+1)+"Bg.png");
+		}
+		vicScreen = new Texture("victoryScreen.png");
+		deadScreen = new Texture("deadScreen.jpg");
 		//sets important rectangles
 		playRect = new Rectangle(335,100,256,128);
 	}
@@ -113,129 +133,59 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 			}
 			batch.end();
 			String buttonClicked = checkClick(); //checks for button clicks
-			if(buttonClicked.equals("play")){
+			if(buttonClicked.equals("play")){ //if the user clicks play
 				startGame();
 			}
 		} if(mode.equals("victory")){
+			//handles victory screen
 			Gdx.gl.glClearColor(0, 0, 0, 1);
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 			batch.begin();
-			font.draw(batch,"YOU WIN!",Gdx.graphics.getWidth()/2-32, Gdx.graphics.getHeight()/2-5);
+			batch.draw(vicScreen,0,-384); //draws the victory screen
 			batch.end();
-			if(savePoint < 0)
+			if(savePoint < 0) //sets savePoint to when the victory screen starts
 				savePoint = time;
-			if(time >= savePoint + 240){
+			if(time >= savePoint + 240){ //after 240 ticks, we restart program to let user play again
 				create();
 			}
 			time++;
 		} else if(mode.equals("dead")){
-			//handles defeat
+			//handles defeat screen
 			Gdx.gl.glClearColor((float)0.7, 0, 0, 1);
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 			batch.begin();
-			font.draw(batch, "GAME OVER", Gdx.graphics.getWidth()/2-32, Gdx.graphics.getHeight()/2-5);
+			batch.draw(deadScreen,0,-384); //draws the dead screen
 			batch.end();
-			if(savePoint < 0)
+			if(savePoint < 0) //sets the savePoint to when the dead screen starts 
 				savePoint = time;
-			if(time >= savePoint + 240){
-				startGame();
+			if(time >= savePoint + 240){ //after 240 ticks, we restart the program to allow user to play again
+				create();
 			}
 			time++;
 		} else if(mode.equals("game")){
-			Gdx.gl.glClearColor((float)0.88, (float)0.5, (float)0.88, 1);
+			Gdx.gl.glClearColor((float)0.3, (float)0.3, (float)0.3, 1);
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-			if(!onPlat){
-				//handles user moving platform
-				ball.move(spd);
-				for(int i = extraBalls.size()-1;i>=0;i--){
-					Ball b = extraBalls.get(i);
-					b.move(spd);
-				}
-				movePlatform();
-				bounce(ball);
-				for(int i = extraBalls.size()-1;i>=0;i--){
-					Ball b = extraBalls.get(i);
-					bounce(b);
-				}
-				checkLose();
+			drawBackground(); //draws background for level
+			if(paused){
+				//if paused we just draw a game state and put the paused sign
+				drawGameState();
+				batch.begin();
+				batch.draw(pausedSign, Gdx.graphics.getWidth()/2-128, Gdx.graphics.getHeight()/2 - 64);
+				batch.end();
+			} else{
+				gameLoop(); //runs main game loop that handles everything gameplay related
 			}
-			//sets position of sprites
-			ballSprite.setPosition((float)ball.getX(), (float)ball.getY());
-			//handles big and small effects before platsprite
-			if(time - savePoint > 1200 && savePoint > 0){
-				//resets small and big
-				platSprite.setScale(1,1);
-				platwidth = 128;
+		} else if(mode.equals("nextLevel")){
+			//moves on to next level after a bit
+			drawBackground();
+			drawGameState();
+			if(time - savePoint > 240){
+				savePoint = time;
 			}
-			platRect.width = (float)platwidth;
-			float npx = (float)platx; //new plat x, it's what the sprite's x becomes
-			if(platwidth == 256){
-				npx += 64;
-			} else if(platwidth == 64){
-				npx -= 32;
-			}
-			platSprite.setPosition(npx,(float)platy);
-			batch.begin();
-			
-			//draws bricks
-			for(int i = bricks.size()-1; i >= 0; i--){
-				Brick b = bricks.get(i);
-				batch.draw(brickSprites[b.getDurability()],(float)b.getX(),(float)b.getY());
-				//draws effects on bricks that have them
-				if(b.getEffect().equals("balls")){
-					batch.draw(ballsEffectSym, (float)b.getX(), (float)b.getY());
-				} else if(b.getEffect().equals("life")){
-					batch.draw(lifeEffectSym,(float)b.getX(), (float)b.getY());
-				} else if(b.getEffect().equals("big")){
-					batch.draw(bigEffectSym, (float)b.getX(), (float)b.getY());
-				} else if(b.getEffect().equals("small")){
-					batch.draw(smallEffectSym, (float)b.getX(), (float)b.getY());
-				}
-			}
-			
-			//draws ball
-			ballSprite.draw(batch);
-			//draws extra balls
-			for(Ball b: extraBalls){
-				Sprite bs = new Sprite(ballSprite.getTexture());
-				bs.setPosition((float)b.getX(), (float)b.getY());
-				bs.draw(batch);
-			}
-			//draws platform
-			platSprite.draw(batch);
-			
-			//draws lives
-			font.draw(batch, "Lives: " + lives, 5, Gdx.graphics.getHeight()-16);
-			if(time < 120){
-				font.draw(batch, "Level " + level, Gdx.graphics.getWidth()/2-16, Gdx.graphics.getHeight()/2);
-			}
-			batch.end();
-			if(onPlat){
-				//if ball on platform adjust angle
-				changeAngle();
-				//draws line
-				drawGuideLine();
-			}
-			//removes all dead bricks
-			for(int i = bricks.size()-1;i >= 0;i--){
-				if(bricks.get(i).isGone()){
-					bricks.remove(i);
-				}
+			if(time - savePoint == 240){
+				startNextLevel();
 			}
 			time++;
-			//handles level success
-			if(bricks.size() <= quota){
-				level++;
-				time = 0;
-				drawLevel();
-				onPlat = true;
-				platx = Gdx.graphics.getWidth()/2-platwidth/2;
-				platRect.x = (float)platx;
-				ball.setX(platx+platwidth/2 - 8);
-				ball.setY(16);
-				ball.setAngle(90);
-				extraBalls = new ArrayList<Ball>();
-			}
 		}
 	}
 	
@@ -256,6 +206,7 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 		return "";
 	}
 	
+	//GAMEPLAY METHODS
 	public void startGame(){
 		//starts game
 		mode = "game";
@@ -277,14 +228,127 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 		savePoint = -1;
 		time = 0;
 		lives = 3; //3 lives
-		spd = 5;
 		quota = 0;
-		drawLevel(); //adds all bricks
+		paused = false;
+		createLevel(); //adds all bricks
 	}
-	
+	public void startNextLevel(){
+		//starts next level
+		bricks.clear(); //resets bricks
+		extraBalls = new ArrayList<Ball>(); //resets balls
+		ball = new Ball(Gdx.graphics.getWidth()/2-8,16,90);
+		level++;
+		time = 0;
+		onPlat = true;
+		platwidth = 128;
+		platRect.width = (float)platwidth;
+		platx = Gdx.graphics.getWidth()/2-platwidth/2;
+		platRect.x = (float)platx;
+		platSprite.setScale(1,1);
+		savePoint = -1;
+		mode = "game";
+		createLevel();
+	}
+
+	public void gameLoop(){
+		if(!onPlat){
+			//handles user moving platform and ball movement
+			ball.normalizeSpeed(time);
+			for(Ball b: extraBalls){
+				b.normalizeSpeed(time);
+			}
+			ball.move();
+			for(int i = extraBalls.size()-1;i>=0;i--){
+				Ball b = extraBalls.get(i);
+				b.move();
+			}
+			movePlatform();
+			bounce(ball);
+			for(int i = extraBalls.size()-1;i>=0;i--){
+				Ball b = extraBalls.get(i);
+				bounce(b);
+			}
+			checkLose();
+		}
+		//sets position of sprites
+		ballSprite.setPosition((float)ball.getX(), (float)ball.getY());
+		//handles big and small effects before platsprite
+		if(time - savePoint > 1200 && savePoint > 0){
+			//resets small and big
+			platSprite.setScale(1,1);
+			platwidth = 128;
+		}
+		platRect.width = (float)platwidth;
+		float npx = (float)platx; //new plat x, it's what the sprite's x becomes
+		if(platwidth == 256){
+			npx += 64;
+		} else if(platwidth == 64){
+			npx -= 32;
+		}
+		platSprite.setPosition(npx,(float)platy);
+		drawGameState(); //draws current game state
+		//removes all dead bricks
+		for(int i = bricks.size()-1;i >= 0;i--){
+			if(bricks.get(i).isGone()){
+				bricks.remove(i);
+			}
+		}
+		time++;
+		//handles level success
+		if(bricks.size() <= quota){
+			mode = "nextLevel"; //sets mode to next Level mode
+		}
+	}
 	//DRAWING METHODS
-	public void drawLevel(){
-		//although it's called drawLevel(), all it really does is add bricks :/
+	public void drawGameState(){
+		//draws the game state, which is the bricks, ball and platform
+		batch.begin();
+		//draws bricks
+		for(int i = bricks.size()-1; i >= 0; i--){
+			Brick b = bricks.get(i);
+			batch.draw(brickSprites[b.getDurability()],(float)b.getX(),(float)b.getY());
+			//draws effects on bricks that have them
+			if(b.getEffect().equals("balls")){
+				batch.draw(ballsEffectSym, (float)b.getX(), (float)b.getY());
+			} else if(b.getEffect().equals("life")){
+				batch.draw(lifeEffectSym,(float)b.getX(), (float)b.getY());
+			} else if(b.getEffect().equals("big")){
+				batch.draw(bigEffectSym, (float)b.getX(), (float)b.getY());
+			} else if(b.getEffect().equals("small")){
+				batch.draw(smallEffectSym, (float)b.getX(), (float)b.getY());
+			} else if(b.getEffect().equals("fast")){
+				batch.draw(fastEffectSym, (float)b.getX(), (float)b.getY());
+			} else if(b.getEffect().equals("slow")){
+				batch.draw(slowEffectSym, (float)b.getX(), (float)b.getY());
+			}
+		}
+		
+		//draws ball
+		ballSprite.draw(batch);
+		//draws extra balls
+		for(Ball b: extraBalls){
+			Sprite bs = new Sprite(ballSprite.getTexture());
+			bs.setPosition((float)b.getX(), (float)b.getY());
+			bs.draw(batch);
+		}
+		//draws platform
+		platSprite.draw(batch);
+		
+		//draws text saying lives
+		font.draw(batch, "Lives: " + lives, 5, Gdx.graphics.getHeight()-16);
+		if(time < 120){
+			font.draw(batch, "Level " + level, Gdx.graphics.getWidth()/2-16, Gdx.graphics.getHeight()/2);
+		}
+		batch.end();
+		if(onPlat){
+			//if ball on platform adjust angle
+			changeAngle();
+			//draws line
+			drawGuideLine();
+		}
+	}
+	public void createLevel(){
+		//creates a level by adding bricks
 		if(level == 1){
 			//draws level 1
 			//add red bricks on bottom
@@ -301,8 +365,10 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 			for(int i = 0; i < 14;i++){
 				if(i == 3){
 					bricks.add(new Brick(i*66+33,384,64,32,2,"balls"));
+				} else if(i == 8){
+					bricks.add(new Brick(i*66+33,384,64,32,2,"fast"));
 				} else{
-					bricks.add(new Brick(i*66+33,384,64,32,2));	
+					bricks.add(new Brick(i*66+33,384,64,32,2));
 				}
 			}
 			//adds stronger bricks on top
@@ -311,7 +377,11 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 					bricks.add(new Brick(i*66+2,468,64,32,3,"life"));
 				} else if(i == 3 || i == 9){
 					bricks.add(new Brick(i*66+2,468,64,32,3,"big"));
-				}	else{
+				} else if(i == 7){
+					bricks.add(new Brick(i*66+2,468,64,32,3,"fast"));
+				} else if(i == 0 || i == 12){
+					bricks.add(new Brick(i*66+2,468,64,32,3,"balls"));
+				} else{
 					bricks.add(new Brick(i*66+2,468,64,32,3));
 				}
 			}
@@ -331,6 +401,8 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 			for(int i = 0; i < 14;i++){
 				if(i == 2 || i == 8){
 					bricks.add(new Brick(i*66+33,384,64,32,2,"big"));
+				} else if(i == 5 || i == 1){
+					bricks.add(new Brick(i*66+33,384,64,32,2,"fast"));
 				} else{
 					bricks.add(new Brick(i*66+33,384,64,32,2));
 				}
@@ -341,13 +413,17 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 					bricks.add(new Brick(i*66+2,468,64,32,3,"small"));
 				} else if(i == 10){
 					bricks.add(new Brick(i*66+2,468,64,32,3,"balls"));
-				}	else{
+				} else if(i == 0 || i == 8){
+					bricks.add(new Brick(i*66+2,468,64,32,3,"slow"));
+				} else{
 					bricks.add(new Brick(i*66+2,468,64,32,3));
 				}
 			}
 			for(int i = 0;i < 14;i++){
 				if(i == 11){
 					bricks.add(new Brick(i*66+33,552,64,32,4,"life"));
+				} else if(i == 3){
+					bricks.add(new Brick(i*66+33,552,64,32,4,"small"));
 				} else{
 					bricks.add(new Brick(i*66+33,552,64,32,4));
 				}
@@ -357,6 +433,7 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 			mode = "victory";
 			savePoint = -1; //resets savePoint so victory can use it again
 		}
+		numBricks = bricks.size(); //number of bricks level had originally
 	}
 	
 	public void drawGuideLine(){
@@ -369,9 +446,24 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 		shapeRenderer.begin(ShapeType.Line);
 		shapeRenderer.setColor(new Color(0,0,0,1));
 		shapeRenderer.line(x1,y1,x2,y2);
+		shapeRenderer.setColor(new Color(1,1,1,1));
+		shapeRenderer.line(x1+1, y1+1, x2+1, y2+1); //draws a white line to avoid invisibility
 		shapeRenderer.end();
 	}
-	
+	public void drawBackground(){
+		if(numBricks - quota <= 0)
+			numBricks = quota+1; //prevents crashing
+		float bgBase = Gdx.graphics.getHeight() - Gdx.graphics.getHeight()*(bricks.size()-quota)/(numBricks-quota);
+
+		batch.begin();
+		batch.draw(backgrounds[level-1],0,-384);//background of level
+		batch.end();
+		shapeRenderer.begin(ShapeType.Filled);
+		//draws a background that gets slowly goes up to reveal everything
+		shapeRenderer.setColor(new Color((float)0.7,(float)0.7,(float)0.7,1));
+		shapeRenderer.rect(0, bgBase, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		shapeRenderer.end();
+	}
 	//POSITION AFFECTING METHODS
 	public void changeAngle(){
 		//changes angle based on arrow keys
@@ -392,10 +484,10 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 	public void movePlatform(){
 		//moves the platform based on input
 		if(Gdx.input.isKeyPressed(Keys.LEFT)){
-			platx -= 7;
+			platx -= ball.getSpeed()+2;
 		}
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)){
-			platx += 7;
+			platx += ball.getSpeed()+2;
 		}
 		if(platx < 0) platx = 0;
 		if(platx > Gdx.graphics.getWidth() - platwidth) platx = Gdx.graphics.getWidth()-platwidth;
@@ -406,10 +498,10 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 		//bounces on bricks or platform or walls
 		//WALLS
 		if(ball.getX() + Ball.WIDTH >= Gdx.graphics.getWidth() || ball.getX() <= 0){
-			ball.move(-spd);
+			ball.move(-1);
 			ball.setAngle(180 - ball.getAngle()+1);
 		} else if(ball.getY() + Ball.HEIGHT >= Gdx.graphics.getHeight()){
-			ball.move(-spd);
+			ball.move(-1);
 			ball.setAngle(-ball.getAngle()+1); //the plus one is to prevent an infinite bounce loop
 		}
 		//PLATFORM
@@ -420,6 +512,7 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 		for(int i = 0;i < bricks.size();i++){
 			Brick b = bricks.get(i);
 			if(b.checkCollision(ball)){
+				brickHit.play(); //plays the brickhit
 				if(!b.isXBounce(ball)){
 					//vertical bounce
 					ball.setAngle(-ball.getAngle()+1);
@@ -431,7 +524,7 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 				if(b.getEffect().equals("balls")){
 					//adds balls to list
 					for(int j = 0;j < 3;j++)
-						extraBalls.add(new Ball(platx+platwidth/2-8,platy+18,(float)(Math.random()*140)+20));
+						extraBalls.add(new Ball(platx+platwidth/2-8,platy+18,(float)(Math.random()*140)+20,ball.getSpeed(),ball.getSpeedCounter()));
 				} else if(b.getEffect().equals("life")){
 					lives++;
 				} else if(b.getEffect().equals("big")){
@@ -444,6 +537,20 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 					platwidth = 64; //halve the width
 					platSprite.setScale((float)0.5,1);
 					savePoint = time;
+				} else if(b.getEffect().equals("fast")){
+					ball.setSpeed(7);
+					ball.setSpeedCounter(time);
+					for(Ball bl: extraBalls){
+						bl.setSpeed(7);
+						bl.setSpeedCounter(time);
+					}
+				} else if(b.getEffect().equals("slow")){
+					ball.setSpeed(3);
+					ball.setSpeedCounter(time);
+					for(Ball bl: extraBalls){
+						bl.setSpeed(3);
+						bl.setSpeedCounter(time);
+					}
 				}
 				if(!b.getEffect().equals("")){
 					b.setEffect("");
@@ -484,10 +591,19 @@ public class Arkanoid extends ApplicationAdapter implements InputProcessor{
 	@Override
 	public boolean keyDown(int keycode) {
 		// TODO Auto-generated method stub
-		if(onPlat){
-			if(keycode == Keys.SPACE){
-				onPlat = false;
+		if(mode.equals("game")){
+			if(onPlat){
+				if(keycode == Keys.SPACE){
+					onPlat = false;
+				}
 			}
+			if(keycode == Keys.P){
+				paused = !paused;
+			}
+		}
+		//CHEATS
+		if(keycode == Keys.L){
+			bricks.clear();
 		}
 		return false;
 	}
--- ArkanoidProject/core/src/com/zhou/arkanoid/Ball.java
+++ ArkanoidProject/core/src/com/zhou/arkanoid/Ball.java
@@ -5,11 +5,20 @@ import com.badlogic.gdx.math.Rectangle;
 class Ball {
 	public static final double WIDTH = 16;
 	public static final double HEIGHT = 16;
-	private double x,y,ang;
+	private double x,y,spd,ang;
+	private int speedCounter; //counter for when speed was last edited - for time based events
 	public Ball(double x, double y, double ang){
 		this.x = x;
 		this.y = y;
 		this.ang = ang;
+		this.spd = 5;
+		speedCounter = -1;
+	}
+	public Ball(double x, double y, double ang, double spd, int time){
+		//for setting speed and speed counter as well
+		this(x,y,ang);
+		this.spd = spd;
+		this.speedCounter = time;
 	}
 	
 	public void move(double dist){
@@ -17,7 +26,19 @@ class Ball {
 		this.x += Math.cos(Math.PI*this.ang/180)*dist;
 		this.y += Math.sin(Math.PI*this.ang/180)*dist;
 	}
-	
+	public void move(int mult){
+		//multiplier to scale speed
+		move((double)mult*spd);
+	}
+	public void move(){
+		move(spd); //moves at spd
+	}
+	public void normalizeSpeed(int time){
+		//normalizes speed based on time
+		if(time - speedCounter >= 1200 && spd != 5){
+			spd = 5;
+		}
+	}
 	//Simple getter methods
 	public double getX(){
 		return x;
@@ -25,14 +46,18 @@ class Ball {
 	public double getY(){
 		return y;
 	}
+	public double getSpeed(){
+		return spd;
+	}
 	public double getAngle(){
 		return ang;
 	}
-	
+	public int getSpeedCounter(){
+		return speedCounter;
+	}
 	public Rectangle getRect(){
 		return new Rectangle((float)x,(float)y,(float)(WIDTH),(float)(HEIGHT));
 	}
-	
 	//simple setter methods
 	public void setX(double x){
 		this.x = x;
@@ -40,9 +65,15 @@ class Ball {
 	public void setY(double y){
 		this.y = y;
 	}
+	public void setSpeed(double spd){
+		this.spd = spd;
+	}
 	public void setAngle(double newAng){
 		//changes the angle
 		this.ang = newAng;
+	}
+	public void setSpeedCounter(int time){
+		speedCounter = time;
 	}
 	
 }
